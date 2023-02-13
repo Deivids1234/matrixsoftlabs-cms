d7cc143468152c261a7a7512748abeca85b52935
DAVIS_ILJINS <davis@matrixsoftlabs.com>
2023-02-13 19:44:55 +0200

    fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,20 @@
 
 module.exports = ({ env }) => [
   'strapi::errors',
-  {
+    {
     name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
+          "connect-src": ["'self'", "https:"],
           "img-src": [
             "'self'",
             "data:",
             "blob:",
-            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
+            "*.digitaloceanspaces.com"
           ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
-          ],
+          "media-src": ["'self'", "data:", "blob:"],
           upgradeInsecureRequests: null,
         },
       },
